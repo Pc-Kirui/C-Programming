f0a2302aeff0c22ea795d44d574223bb37647359
Pc Kirui <patokirui@gmail.com>
2022-10-12 10:07:07 +0300

    added a new file to pointers

---
+++ pointersInC/menuDrivenSystem.c
@@ -0,0 +1,31 @@
+//array of pointers to functions
+#include<stdio.h>
+
+void function1(int a);
+void function2(int b);
+void function3(int c);
+int main()
+{
+    //initiolizing array of 3 pointers to functions that each take an int as and argument
+    void(*f[3])(int) = {function1,function2,function3};
+    int choice;
+    printf("Enter a number between 0 and 2,3 to end:\n");
+    scanf("%d",&choice);
+
+    while(choice >= 0 && choice < 3){
+        (*f[choice])(choice);
+        printf("Enter a number between 0 and 2,3 to end:\n");
+        scanf("%d",&choice);
+    }
+    printf("Program execution completed.\n");
+    return 0;
+}
+void function1(int a){
+    printf("You entered %d so function1 was called\n\n",a);
+}
+void function2(int b){
+    printf("You entered %d so function2 was called\n\n",b);
+}
+void function3(int c){
+    printf("You entered %d so function3 was called\n\n",c);
+}
